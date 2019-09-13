1d7e10e539445ea45aa022dff60f0c27dd7aeff5
AliAbdulle <alidhobey@gmail.com>
2019-09-13 16:04:20 -0500

    first commit

---
+++ ProjectTestCSharp/ProjectTestCSharp/TestClass.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTestCSharp
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestMethod()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://www.facebook.com";
+            IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='email']"));
+            emailTextField.SendKeys("OpenSeleniumC#");
+            driver.Quit();
+
+        }
+
+    }
+}
