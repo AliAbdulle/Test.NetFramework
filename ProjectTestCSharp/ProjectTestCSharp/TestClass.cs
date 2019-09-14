20d9571def5a9c2963183e3b8d44b74efbcaf1a3
AliAbdulle <alidhobey@gmail.com>
2019-09-13 20:11:59 -0500

    Add new folder

--- ProjectTestCSharp/ProjectTestCSharp/TestClass.cs
+++ ProjectTestCSharp/ProjectTestCSharp/TestClass.cs
@@ -6,22 +6,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ProjectTestCSharp.BaseClass;
+using System.Threading;
+using OpenQA.Selenium.Support.UI;
 
 namespace ProjectTestCSharp
 {
     [TestFixture]
-    public class TestClass
+    public class TestClass : BaseTest
     {
-        [Test]
-        public void TestMethod()
+        [Test, Category("Smoke Testing")]
+        public void TestMethod1()
         {
-            IWebDriver driver = new ChromeDriver();
-            driver.Url = "https://www.facebook.com";
+          
             IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='email']"));
             emailTextField.SendKeys("OpenSeleniumC#");
-            driver.Quit();
+            IWebElement monthDropdownList = driver.FindElement(By.XPath(".//*[@id='month']"));
+            SelectElement element = new SelectElement(monthDropdownList);
+            element.SelectByIndex(1);
+            element.SelectByText("Jun");
+            element.SelectByValue("6");
 
         }
+
+        [Test, Category("Regression Testing")]
+        public void TestMethod2()
+        {
+
+            IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='email']"));
+            emailTextField.SendKeys("OpenSeleniumC#");
+            Thread.Sleep(5000);
+
+        }
+
+        [Test, Category("Regression Testing")]
+        public void TestMethod3()
+        {
+
+            IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='email']"));
+            emailTextField.SendKeys("OpenSeleniumC#");
+
+
+        }
+
 
     }
 }
